1de15c31ab241987a8231de8365498b7a6075420
WiemerSteenbeek <wiemer.steenbeek@gmail.com>
2024-06-30 11:02:38 +0200

    Documentation updated

--- readme.md
+++ readme.md
@@ -3,30 +3,16 @@ Setup:
 1. If you don't have npm or Typescript installed yet, follow the instructions described in section 3 of https://docs.mendix.com/apidocs-mxsdk/mxsdk/setting-up-your-development-environment/#setting. You can ignore the steps before and after.
 2. Either download this project or click on 'Use this template' to immediately create your own repository based on this template.
 3. Create a folder named 'config' in this project's root folder.
-4. In it, put a file named 'config.json' that contains the following JSON snippet:
-
-   {
-   "projectid": "{YourProjectIdHere}",
-   "mendixtoken": "{YourMendixTokenHere}",
-   "branch": "main"
-   }
-
-5. Replace {YourProjectIdHere} with the ID of the project you want to use. You can find it in Sprintr under General > Settings.
-6. Replace {YourMendixTokenHere} with one you have generated (or create at https://user-settings.mendix.com/link/developersettings)
-7. Set the branch that you want to use. The main line for GIT-projects is 'main', for SVN-projects this is 'trunk'.
-8. (Optional) Replace the branch name if you don't want to use the main branch of your project
-9. From your terminal within the project directory, run the command 'npm i'. This will install all the necessary node modules.
+4. Copy the files in the folder 'config_preset' to the config folder
+5. Create your own PAT at https://user-settings.mendix.com/link/developersettings and provide it in the pat.json file
+6. Provide a list of apps that you want to analyze in apps.json. An app's ID can be found in Sprintr under Settings. If you want to analyze a different branch, you can change it here as well. For SVN-projects, use 'trunk' as the default branch name.
+7. From your terminal within the project directory, run the command 'npm i'. This will install all the necessary node modules.
 
 ---
 
 Instructions:
 
-This basic project allows you to open a working copy of a Mendix project and more or less do whatever you want with it: analyze, build, automate...
-You can use the following npm-commands:
-
-- npm run build: compile (actually: transpile) your TypeScript to JavaScript
-- npm run script: execute the JavaScript file index.js that you created using 'build'
-- npm run buildandrun: transpile your TS to JS and immediately execute
+The command 'npm run dojofinder' will run your analysis immediately in one go. Other commands (clean, only transpile etc.) can be found in package.json.
 
 ---
 
--- src/dojofinder.ts
+++ src/dojofinder.ts
@@ -2,40 +2,40 @@ import { MendixPlatformClient, setPlatformConfig } from "mendixplatformsdk";
 import * as fs from "fs";
 import { JavaScriptSerializer, pages } from "mendixmodelsdk";
 
-const config = require("../config/config.json");
+const pat = require("../config/pat.json").token as string;
 const apps = require("../config/apps.json");
-const regexes = require("../config/regexes.json");
+const regexList = transformList(require("../config/regexes.json"));
 
 main().catch(console.error);
 
 async function main() {
   // 1. Set up your Mendix Model SDK client with your Mendix PAT:
-  setPlatformConfig({ mendixToken: config.mendixtoken });
+  setPlatformConfig({ mendixToken: pat });
   const mxClient = new MendixPlatformClient();
 
-  const regexList = transformList(regexes);
-
-  // Iterate over your list of projects:
+  // Iterate over your list of apps:
   for (const app of apps) {
-    // 2. Load the project:
-    console.log(`Opening project: ${app.name} with id: ${app.id} and branch: ${app.branch}`);
+    // 2. Load the app:
+    console.log(`Opening app ${app.name} with id ${app.id} and branch ${app.branch}`);
     const myMendixApp = mxClient.getApp(app.id);
     const mxWorkingCopy = await myMendixApp.createTemporaryWorkingCopy(app.branch);
     const myMendixModel = await mxWorkingCopy.openModel();
+    console.log(`Opened app ${app.name}`);
 
-    // 3. Get all documents in the project that are pages, layouts or snippets:
+    // 3. Get all documents in the app that are pages, layouts or snippets:
     const documents = myMendixModel.allDocuments().filter((document) => document instanceof pages.Page || document instanceof pages.Layout || document instanceof pages.Snippet);
+    console.log(`Found ${documents.length} documents in app ${app.name}`);
 
     // Create a (empty) list of documents that contain a reference to a pluggable widget
     let documentsWithDojoWidgets: { documentType: string; documentName: string; widgetType: string; widgetName: string }[] = [];
-
+    console.log(`Checking all documents for Dojo widgets in app ${app.name}`);
     for (const document of documents) {
       // 4. Get JS code that would recreate this document
-      const jsOutput = JavaScriptSerializer.serializeToJs(await document.load());
+      const serializedDocument = JavaScriptSerializer.serializeToJs(await document.load());
 
-      // 5. Check the serialized document against all regexes
+      // 5. Check the generated document against a list of regular expressions
       for (const regex of regexList) {
-        const referenceMatch = jsOutput.matchAll(regex.regex);
+        const referenceMatch = serializedDocument.matchAll(regex.regex);
         // 6. If there is a match, add the document to the list
         for (const match of referenceMatch) {
           const output = match[0];
@@ -52,6 +52,8 @@ async function main() {
         }
       }
     }
+
+    console.log(`Found ${documentsWithDojoWidgets.length} outdated widgets in app ${app.name}`);
 
     // Write the resulting output to a file:
     fs.writeFileSync(`output/${app.name}.json`, JSON.stringify(documentsWithDojoWidgets));
